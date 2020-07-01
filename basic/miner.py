ddeda93b5339e88afc1eebc37dc8fbb1d419a1ac
KIMSUBIN <xxxia@naver.com>
2020-07-01 10:44:26 +0900

    200701 10:44
    konlpy

--- basic/miner.py
+++ basic/miner.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
-
+import nltk
+from konlpy.tag import Okt
+import re
 
 @dataclass
-class Model:
+class Entity:
     context: str
     fname: str
     target: str
@@ -27,11 +29,83 @@ class Model:
 
 class Service:
     def __init__(self):
-        pass
+        self.texts = []
+        self.tokens = []
+        self.okt = Okt()
+        self.stopwords = []
+        self.freqtxt = []
+
+    def extract_token(self, payload):
+        print('>> 1. corpus 에서 token 추출')
+        filename = payload.context + payload.fname
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.texts = f.read()
+        print(f'1단계 결과물: {self.texts[:300]}')
+
+    def extract_hanguel(self):
+        print('>> 2. corpus 에서 한글 추출')
+        texts = self.texts.replace('\n', ' ')
+        tokenizer = re.compile(r'[^ㄱ-힣]')
+        # ^ 는 not 과 start 두가지 개념이 있음
+        # [^] 는 not, ^[]는 start 의미로 표현됨
+        self.texts = tokenizer.sub('', texts)
+        # 한글이 아닌 것은 '' 처리해서 한글만 남겨라
+        print(f'2단계 결과물: {self.texts[:300]}')
+
+    def conversion_token(self):
+        print('>> 3. 한글 token 변환')
+
+    def compound_noun(self):
+        print('>> 4. 복합명사화')
+
+    def extract_stopword(self):
+        print('>> 5. 노이즈 코퍼스에서 토큰 추출')
+
+    def filtering_text_with_stopword(self):
+        print('>> 6. 노이즈 필터링 후 시그널 추출')
+
+    def freqtxt_text(self):
+        print('>> 7. 시그널 중에 사용빈도 정렬')
+
+    def wordcloud(self):
+        print('>> 8. 시각화')
 
 
 class Controller:
     def __init__(self):
         pass
 
+    def download_dictionary(self):
+        nltk.download('all')
 
+    def data_analysis(self):
+        entity = Entity()
+        service = Service()
+        service.extract_token()
+        service.extract_hanguel()
+        service.conversion_token()
+        service.compound_noun()
+        service.extract_stopword()
+        service.filtering_text_with_stopword()
+        service.freqtxt_text()
+        service.wordcloud()
+
+
+def print_menu():
+    print('0. Exit\n')
+    print('1. 사전 다운로드\n')
+    return input('메뉴선택\n')
+
+
+app = Controller()
+
+while 1:
+    menu = print_menu()
+    if menu == '1':
+        app.download_dictionary()
+
+    if menu == '0':
+        break
+
+    if menu == '2':
+        app.data_analysis()
